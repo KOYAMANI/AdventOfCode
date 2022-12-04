6b4b11ad05d39d2626b77b4deea3fc36e413599f
KOYAMANI <ykkg3ym1114@gmail.com>
2022-12-04 08:59:38 +0100

    day03 update

--- day03/solution.py
+++ day03/solution.py
@@ -3,8 +3,8 @@ Advent of Code 2022
 --- Day 3: Rucksack Reorganization ---
 """
 
-# ord('a') - ord('z'): 97 - 122 : priority 1-26 (-96)
-# ord('A') - ord('Z'): 65 - 90 : priority 27-52 (-38)
+# ord('a') - ord('z') : 97 - 122 -> priority : 1-26 (-96)
+# ord('A') - ord('Z') : 65 - 90 -> priority : 27-52 (-38)
 
 
 def get_input(filename):
@@ -18,7 +18,10 @@ def solve_part1(rucksacks):
         length = len(line)
         s1 = line[0:length // 2]
         s2 = line[length // 2:length]
-        group = list(set(s1) & set(s2))
+        # Set intersection Time complexity (s1 & s2)
+        # Average: O(min(len(s1), len(2)))
+        # Worst: O(len(s1) * len(s2))
+        group = set(s1) & set(s2)
         for c in group:
             print("The common letters are:", c)
             if c == '\n':
@@ -33,10 +36,11 @@ def solve_part1(rucksacks):
 def solve_part2(rucksacks):
     priorities = 0
     for i in range(0, len(rucksacks), 3):
-        group = list(set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2]))
-        print(group)
+        # Set intersection Time complexity (s1 & s2 ... & sn)
+        # Worst: (n-1)*O(l) where l is max(len(s1),..,len(sn))
+        group = set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2])
         for c in group:
-            print("The common letters are:", c)
+            # print("The common letters are:", c)
             if c == '\n':
                 continue
             elif c.islower():
